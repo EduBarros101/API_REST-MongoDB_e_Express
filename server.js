5c1921d5df90455f2e657e22f6614565361bf410
Eduardo Barros <edu.barros101@gmail.com>
2023-05-22 11:25:54 -0300

    Patch: comentários removidos.

--- server.js
+++ server.js
@@ -2,19 +2,6 @@ import app from './src/app.js'
 
 const port = process.env.PORT || 3000
 
-// const rotas = {
-//   '/': 'Curso de NodeJS.',
-//   '/livros': 'Página de livros.',
-//   '/autores': 'Lista de autores.',
-//   '/editora': 'Página de editora.',
-//   '/sobre': 'Informações sobre o projeto.',
-// }
-
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/plain' })
-//   res.end(rotas[req.url])
-// })
-
 app.listen(port, () => {
   console.log(`Servidor escutando em http://localhost:${port}.`)
 })
